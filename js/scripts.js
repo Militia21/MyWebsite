fbe82e1e69aeda089d328cf70d4518ad9e176cf1
Militia21 <70501605+Militia21@users.noreply.github.com>
2021-01-25 12:16:46 +1100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -78,3 +78,17 @@ function showPage() {
   
   })(jQuery); // End of use strict
   
+
+/*function check(e)
+{
+alert(e.keyCode);
+}*/
+ document.onkeydown = function(e) {
+    if (e.ctrlKey && (e.keyCode === 67 || e.keyCode === 86 || e.keyCode === 85 ||     e.keyCode === 117 || e.keycode === 17 || e.keycode === 85)) {//ctrl+u Alt+c, Alt+v will also be disabled sadly.
+        alert('Imagine being a copy cat! ');
+    }
+    return false;
+};
+
+document.addEventListener('contextmenu', event => event.preventDefault());
+
